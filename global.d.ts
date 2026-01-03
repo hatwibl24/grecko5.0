62edc583b5724c5af542b7fa8f93ba9c7a6bc560
hatwibl24 <lubegahatwib13@gmail.com>
2026-01-03 19:35:05 +0300

    Extend JSX for three/fiber elements in global.d.ts
    Updated global.d.ts to extend JSX with three/fiber elements and added type references.

--- global.d.ts
+++ global.d.ts
@@ -1,15 +1,19 @@
-// global.d.ts
-declare namespace JSX {
-  interface IntrinsicElements {
-    mesh: any
-    group: any
-    icosahedronGeometry: any
-    torusGeometry: any
-    meshStandardMaterial: any
-    meshBasicMaterial: any
-    ambientLight: any
-    pointLight: any
-    Edges: any
-    Grid: any
+/// <reference types="vite/client" />
+
+import * as THREE from 'three'
+
+// Extend JSX to include three/fiber elements
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      mesh: React.DetailedHTMLProps<any, any> & { ref?: React.Ref<THREE.Mesh> }
+      group: React.DetailedHTMLProps<any, any> & { ref?: React.Ref<THREE.Group> }
+      icosahedronGeometry: any
+      torusGeometry: any
+      meshStandardMaterial: any
+      meshBasicMaterial: any
+      ambientLight: any
+      pointLight: any
+    }
   }
 }
