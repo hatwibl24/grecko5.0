90da031e51bdfaef9701c53a4bcecd1ce0815b95
hatwibl24 <lubegahatwib13@gmail.com>
2026-01-03 19:20:02 +0300

    Add global TypeScript definitions for JSX elements

---
+++ global.d.ts
@@ -0,0 +1,15 @@
+// global.d.ts
+declare namespace JSX {
+  interface IntrinsicElements {
+    mesh: any
+    group: any
+    icosahedronGeometry: any
+    torusGeometry: any
+    meshStandardMaterial: any
+    meshBasicMaterial: any
+    ambientLight: any
+    pointLight: any
+    Edges: any
+    Grid: any
+  }
+}
